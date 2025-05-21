21e40cdf26b5ef59f51181ac2afd7b2a35cdaf0e
Michael Sena <mike@msena.com>
2025-05-20 20:27:37 -0700

    tests pass

--- Tests/LonghairTests/InterfaceTests.swift
+++ Tests/LonghairTests/InterfaceTests.swift
@@ -2,8 +2,8 @@ import CLonghair
 import Testing
 
 @Test func verifyAPICompat() async throws {
-  if _cauchy_256_init(CAUCHY_256_VERSION) == 0 { 
+  print(CAUCHY_256_VERSION)
+  if _cauchy_256_init(CAUCHY_256_VERSION) != 0 {
     Issue.record("Cauchy 256 init failed")
-   }
-  
+   } 
 }
